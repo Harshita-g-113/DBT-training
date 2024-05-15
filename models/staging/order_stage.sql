2ff49e2722e10919cfa3e098f32e3f6f1f08e45d
harshita152010@gmail.com <harshita152010@gmail.com>
2024-05-15 11:55:41 +0000

    developing stage orders

---
+++ models/staging/order_stage.sql
@@ -0,0 +1,21 @@
+select 
+o.orderId ,
+o.orderdate,
+o.shipdate,
+o.shipmode, 
+o.ordersellingprice - o.ordercostprice AS orderprofit ,
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+
+
+from {{ ref('raw_orders') }} as o
+
+left join {{ ref('raw_customers') }} as c
+on o.customerid = c.customerid
+
+left join {{ ref('raw_products') }} as p
+on o.productId = p.productId
